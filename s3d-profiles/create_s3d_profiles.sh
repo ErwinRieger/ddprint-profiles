b896421ddb030ad8f1989d828ba57157c0998b80
ErwinRieger <erwin.rieger@ibrieger.de>
2021-11-10 16:49:39 +0100

    Import S3D profile generation script.

---
+++ s3d-profiles/create_s3d_profiles.sh
@@ -0,0 +1,32 @@
+
+for nozzle in "40" "80"; do
+
+  for machine in jennyprinter um2 ender5; do
+
+    resultProfile="./${machine}-n${nozzle}.fff"
+
+    echo "Generationg S3D profile for $machine (nozzle: 0.${nozzle}mm) in $resultProfile"
+
+    # Cut ending tag
+    grep -v "</profile>" common.fff > $resultProfile
+
+    # Add nozzle settings
+    cat nozzle${nozzle}.add >> $resultProfile
+
+    # Add machine settings
+    cat machine-${machine}.add >> $resultProfile
+
+    # Add material settings
+    cat material.add >> $resultProfile
+
+    # Add quality settings
+    cat qual-n${nozzle}.add >> $resultProfile
+
+    # Add ending tag
+    echo "</profile>" >> $resultProfile
+
+    # Modify name
+    sed -i "s|Common-N80|${machine}-n${nozzle}|" $resultProfile
+  done
+done
+
